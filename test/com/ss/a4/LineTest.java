d9fa0449be09efc05f42e3df24a1f62579051249
Jacob Fleming <jacob.fleming@smoothstack.com>
2021-11-05 14:37:46 -0400

    updated tests

--- test/com/ss/a4/LineTest.java
+++ test/com/ss/a4/LineTest.java
@@ -1,40 +1,42 @@
 package com.ss.a4;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
 
-@RunWith(JUnit4.class)
+
 class LineTest {
 
-	private Line line;
+	static Line line;
 
-//	@Before
-//	public void init() {
-//		line = new Line(0, 0, 2, 3);
-//	}
-
-	@Test
-	void testGetSlope() {
+	@BeforeEach
+	public void init() {
 		line = new Line(0, 0, 2, 3);
-		assertEquals(1.5, line.getSlope(), .001);
 	}
 
 	@Test
+	void testGetSlope() {
+		assertEquals(1.5, line.getSlope(), .001);
+		
+		line = new Line(0, 0, 0, 3);
+		assertThrows(ArithmeticException.class, ()->{
+			line.getSlope();
+		});
+	}
+
+	@Test	
 	void testGetDistance() {
-		line = new Line(0, 0, 2, 3);
 		assertEquals(3.605, line.getDistance(), .001);
 	}
 
 	@Test
 	void testParallelTo() {
-		line = new Line(0, 0, 2, 3);
 		Line line2 = new Line(1, 0, 3, 3);
 		assertEquals(true, line.parallelTo(line2));
+		line2 = new Line(0,0,3,3);
+		assertEquals(false, line.parallelTo(line2));
 	}
 
 }
